28a616d2938a9dcddf0024c7c2b99a5d44f279ad
Ruven Fehling <r.fehling@mittwald.de>
2013-05-16 16:33:34 +0200

    [TASK] Update GeSHI to version 1.0.8.11

--- Resources/Private/Libraries/GeSHi/contrib/example.php
+++ Resources/Private/Libraries/GeSHi/contrib/example.php
@@ -6,7 +6,7 @@
  * and the language files in subdirectory "../geshi/")
  *
  * @author  Nigel McNie
- * @version $Id$
+ * @version $Id: example.php 2510 2012-06-27 15:57:55Z reedy_boy $
  */
 header('Content-Type: text/html; charset=utf-8');
 
