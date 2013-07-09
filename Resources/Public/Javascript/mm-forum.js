2699ba6b9605cebb309968e4b291a94b4b33fb45
Ruven Fehling <r.fehling@mittwald.de>
2013-07-09 13:03:01 +0200

    [TASK] Autocomplete Function for tags alpha

--- Resources/Public/Javascript/mm-forum.js
+++ Resources/Public/Javascript/mm-forum.js
@@ -34,4 +34,84 @@ $(document).ready(function () {
 			}
 		});
 	});
+
+	$("#tx-mmforum-tag-autocomplete")
+	.bind( "keydown", function( event ) {
+		if ( event.keyCode === $.ui.keyCode.TAB &&
+			$( this ).data( "ui-autocomplete" ).menu.active ) {
+			event.preventDefault();
+		}
+	})
+	.autocomplete({
+		minLength: 0,
+		source: function( request, response ) {
+			$.getJSON( "search.php", {
+				term: extractLast( request.term )
+			}, response );
+		},
+		search: function() {
+			// custom minLength
+			var term = extractLast( this.value );
+			if ( term.length < 2 ) {
+				return false;
+			}
+		},
+		focus: function() {
+			// prevent value inserted on focus
+			return false;
+		},
+		select: function( event, ui ) {
+			var terms = split( this.value );
+			// remove the current input
+			terms.pop();
+			// add the selected item
+			terms.push( ui.item.value );
+			// add placeholder to get the comma-and-space at the end
+			terms.push( "" );
+			this.value = terms.join( ", " );
+			return false;
+		}
+	});
 });
+
+
+function split( val ) {
+	return val.split( /,\s*/ );
+}
+function extractLast( term ) {
+	return split( term ).pop();
+}
+
+
+/**
+	$("#tx-mmforum-tag-autocomplete").autocomplete({
+		source: function (request, response) {
+			$.getJSON("http://ws.geonames.org/searchJSON", {
+				term: extractLast(request.term)
+			}, response);
+		},
+		search: function () {
+			// custom minLength
+			var term = extractLast(this.value);
+			if (term.length < 1) {
+				return false;
+			}
+		},
+		focus: function () {
+			// prevent value inserted on focus
+			return false;
+		},
+		select: function( event, ui ) {
+			var terms = split(this.value);
+			// remove the current input
+			terms.pop();
+			// add the selected item
+			terms.push(ui.item.value);
+			// add placeholder to get the comma-and-space at the end
+			terms.push("");
+			this.value = terms.join(", ");
+			return false;
+		}
+	});**/
+
+
