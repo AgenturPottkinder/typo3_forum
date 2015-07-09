495dbfbeef0fe21622c0b9fcda6209b0fdaa27c9
Sebastian Michaelsen <sebastian@michaelsen.io>
2015-07-09 09:00:21 +0200

    [TASK] Move report model table config to TCA folder

--- ext_tables.php
+++ ext_tables.php
@@ -21,53 +21,38 @@ if (TYPO3_MODE === 'BE') {
 
 
 	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-		'Mittwald.Typo3Forum', 'web', 'tx_typo3forum_m1', '', array('Backend' => 'indexForum', 'Forum' => 'update'),
-		array(
+		'Mittwald.Typo3Forum', 'web', 'tx_typo3forum_m1', '', ['Backend' => 'indexForum', 'Forum' => 'update'],
+		[
 			'access' => 'user,group',
 			'icon' => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
 			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod.xml',
 			'navigationComponentId' => 'typo3-pagetree',
-		)
+		]
 	);
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'typo3_forum');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo3forum_domain_model_moderation_report');
-$TCA['tx_typo3forum_domain_model_moderation_report'] = array(
-	'ctrl' => array(
-		'title' => 'LLL:EXT:typo3_forum/Resources/Private/Language/locallang_db.xml:tx_typo3forum_domain_model_moderation_report',
-		'label' => 'post',
-		'type' => 'type',
-		'tstamp' => 'tstamp',
-		'crdate' => 'crdate',
-		'delete' => 'deleted',
-		'enablecolumns' => array(
-			'disabled' => 'hidden'
-		),
-		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Moderation/Report.php',
-		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/Moderation/Report.png'
-	)
-);
+
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo3forum_domain_model_moderation_reportcomment');
-$TCA['tx_typo3forum_domain_model_moderation_reportcomment'] = array(
-	'ctrl' => array(
+$TCA['tx_typo3forum_domain_model_moderation_reportcomment'] = [
+	'ctrl' => [
 		'title' => 'LLL:EXT:typo3_forum/Resources/Private/Language/locallang_db.xml:tx_typo3forum_domain_model_moderation_reportcomment',
 		'label' => 'text',
 		'tstamp' => 'tstamp',
 		'delete' => 'deleted',
-		'enablecolumns' => array(
+		'enablecolumns' => [
 			'disabled' => 'hidden'
-		),
+		],
 		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Moderation/ReportComment.php',
 		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/Moderation/ReportComment.png'
-	)
-);
+	]
+];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo3forum_domain_model_moderation_reportworkflowstatus');
-$TCA['tx_typo3forum_domain_model_moderation_reportworkflowstatus'] = array(
-	'ctrl' => array(
+$TCA['tx_typo3forum_domain_model_moderation_reportworkflowstatus'] = [
+	'ctrl' => [
 		'title' => 'LLL:EXT:typo3_forum/Resources/Private/Language/locallang_db.xml:tx_typo3forum_domain_model_moderation_reportworkflowstatus',
 		'label' => 'name',
 		'type' => 'login_level',
@@ -78,30 +63,30 @@ $TCA['tx_typo3forum_domain_model_moderation_reportworkflowstatus'] = array(
 		'origUid' => 't3_origuid',
 		'languageField' => 'sys_language_uid',
 		'delete' => 'deleted',
-		'enablecolumns' => array(
+		'enablecolumns' => [
 			'disabled' => 'hidden'
-		),
+		],
 		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Moderation/ReportWorkflowStatus.php',
 		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/Moderation/ReportWorkflowStatus.png'
-	)
-);
+	]
+];
 
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo3forum_domain_model_forum_criteria');
-$TCA['tx_typo3forum_domain_model_forum_criteria'] = array(
-	'ctrl' => array(
+$TCA['tx_typo3forum_domain_model_forum_criteria'] = [
+	'ctrl' => [
 		'title' => 'LLL:EXT:typo3_forum/Resources/Private/Language/locallang_db.xml:tx_typo3forum_domain_model_forum_criteria',
 		'label' => 'name',
 		'tstamp' => 'tstamp',
 		'delete' => 'deleted',
 		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/Forum/Criteria.png',
 		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Forum/Criteria.php',
-	)
-);
+	]
+];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo3forum_domain_model_forum_criteria_options');
-$TCA['tx_typo3forum_domain_model_forum_criteria_options'] = array(
-	'ctrl' => array(
+$TCA['tx_typo3forum_domain_model_forum_criteria_options'] = [
+	'ctrl' => [
 		'title' => 'LLL:EXT:typo3_forum/Resources/Private/Language/locallang_db.xml:tx_typo3forum_domain_model_forum_criteria_options',
 		'label' => 'name',
 		'tstamp' => 'tstamp',
@@ -109,24 +94,24 @@ $TCA['tx_typo3forum_domain_model_forum_criteria_options'] = array(
 		'sortby' => 'sorting',
 		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/Forum/CriteriaOption.png',
 		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Forum/CriteriaOption.php',
-	)
-);
+	]
+];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo3forum_domain_model_forum_ads');
-$TCA['tx_typo3forum_domain_model_forum_ads'] = array(
-	'ctrl' => array(
+$TCA['tx_typo3forum_domain_model_forum_ads'] = [
+	'ctrl' => [
 		'title' => 'LLL:EXT:typo3_forum/Resources/Private/Language/locallang_db.xml:tx_typo3forum_domain_model_forum_ads',
 		'label' => 'name',
 		'tstamp' => 'tstamp',
 		'delete' => 'deleted',
 		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/Forum/Ads.png',
 		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Forum/Ads.php',
-	)
-);
+	]
+];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo3forum_domain_model_user_privatemessages');
-$TCA['tx_typo3forum_domain_model_user_privatemessages'] = array(
-	'ctrl' => array(
+$TCA['tx_typo3forum_domain_model_user_privatemessages'] = [
+	'ctrl' => [
 		'title' => 'LLL:EXT:typo3_forum/Resources/Private/Language/locallang_db.xml:tx_typo3forum_domain_model_user_pm',
 		'label' => 'uid',
 		'tstamp' => 'tstamp',
@@ -134,33 +119,33 @@ $TCA['tx_typo3forum_domain_model_user_privatemessages'] = array(
 		'delete' => 'deleted',
 		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/User/pm.png',
 		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/User/PrivateMessages.php',
-	)
-);
+	]
+];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo3forum_domain_model_user_privatemessages_text');
-$TCA['tx_typo3forum_domain_model_user_privatemessages_text'] = array(
-	'ctrl' => array(
+$TCA['tx_typo3forum_domain_model_user_privatemessages_text'] = [
+	'ctrl' => [
 		'title' => 'LLL:EXT:typo3_forum/Resources/Private/Language/locallang_db.xml:tx_typo3forum_domain_model_user_pm_text',
 		'label' => 'uid',
 		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/User/pm.png',
 		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/User/PrivateMessages_Text.php',
-	)
-);
+	]
+];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo3forum_domain_model_user_rank');
-$TCA['tx_typo3forum_domain_model_user_rank'] = array(
-	'ctrl' => array(
+$TCA['tx_typo3forum_domain_model_user_rank'] = [
+	'ctrl' => [
 		'title' => 'LLL:EXT:typo3_forum/Resources/Private/Language/locallang_db.xml:tx_typo3forum_domain_model_user_rank',
 		'label' => 'name',
 		'sortby' => 'point_limit',
 		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/User/rank.png',
 		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/User/Rank.php',
-	)
-);
+	]
+];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo3forum_domain_model_user_notification');
-$TCA['tx_typo3forum_domain_model_user_notification'] = array(
-	'ctrl' => array(
+$TCA['tx_typo3forum_domain_model_user_notification'] = [
+	'ctrl' => [
 		'title' => 'LLL:EXT:typo3_forum/Resources/Private/Language/locallang_db.xml:tx_typo3forum_domain_model_user_notification',
 		'label' => 'uid',
 		'label_alt' => 'feuser,crdate',
@@ -171,13 +156,13 @@ $TCA['tx_typo3forum_domain_model_user_notification'] = array(
 		'sortby' => 'crdate DESC',
 		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/User/notification.png',
 		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/User/Notification.php',
-	)
-);
+	]
+];
 
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo3forum_domain_model_stats_summary');
-$TCA['tx_typo3forum_domain_model_stats_summary'] = array(
-	'ctrl' => array(
+$TCA['tx_typo3forum_domain_model_stats_summary'] = [
+	'ctrl' => [
 		'title' => 'LLL:EXT:typo3_forum/Resources/Private/Language/locallang_db.xml:tx_typo3forum_domain_model_stats_summary',
 		'label' => 'type',
 		'label_alt' => 'tstamp',
@@ -187,13 +172,13 @@ $TCA['tx_typo3forum_domain_model_stats_summary'] = array(
 		'sortby' => 'tstamp DESC',
 		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/Stats/summary.png',
 		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Stats/Summary.php',
-	)
-);
+	]
+];
 
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo3forum_domain_model_forum_tag');
-$TCA['tx_typo3forum_domain_model_forum_tag'] = array(
-	'ctrl' => array(
+$TCA['tx_typo3forum_domain_model_forum_tag'] = [
+	'ctrl' => [
 		'title' => 'LLL:EXT:typo3_forum/Resources/Private/Language/locallang_db.xml:tx_typo3forum_domain_model_forum_tag',
 		'label' => 'name',
 		'tstamp' => 'tstamp',
@@ -201,5 +186,5 @@ $TCA['tx_typo3forum_domain_model_forum_tag'] = array(
 		'delete' => 'deleted',
 		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/Forum/Tag.png',
 		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Forum/Tag.php',
-	)
-);
+	]
+];
