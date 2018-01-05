878e563590dd501d2b151a9e83640261cd03296e
Kevin Purrmann <kpurrmann@Kevins-MBP.fritz.box>
2018-01-05 11:21:16 +0100

    #218 Add comments

--- Classes/Service/SettingsHydrator.php
+++ Classes/Service/SettingsHydrator.php
@@ -1,5 +1,28 @@
 <?php
 
+/*                                                                      *
+ *  COPYRIGHT NOTICE                                                    *
+ *                                                                      *
+ *  (c) 2017 Mittwald CM Service GmbH & Co KG                           *
+ *           All rights reserved                                        *
+ *                                                                      *
+ *  This script is part of the TYPO3 project. The TYPO3 project is      *
+ *  free software; you can redistribute it and/or modify                *
+ *  it under the terms of the GNU General Public License as published   *
+ *  by the Free Software Foundation; either version 2 of the License,   *
+ *  or (at your option) any later version.                              *
+ *                                                                      *
+ *  The GNU General Public License can be found at                      *
+ *  http://www.gnu.org/copyleft/gpl.html.                               *
+ *                                                                      *
+ *  This script is distributed in the hope that it will be useful,      *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *
+ *  GNU General Public License for more details.                        *
+ *                                                                      *
+ *  This copyright notice MUST APPEAR in all copies of the script!      *
+ *                                                                      */
+
 namespace Mittwald\Typo3Forum\Service;
 
 
@@ -15,11 +38,19 @@ class SettingsHydrator
      */
     private $configurationBuilder;
 
+    /**
+     * injectConfigurationBuilder.
+     * @param ConfigurationBuilder $configurationBuilder
+     */
     public function injectConfigurationBuilder(\Mittwald\Typo3Forum\Configuration\ConfigurationBuilder $configurationBuilder)
     {
         $this->configurationBuilder = $configurationBuilder;
     }
 
+    /**
+     * hydrateSettings.
+     * @param DomainObjectInterface $object
+     */
     public function hydrateSettings(DomainObjectInterface $object)
     {
         if ($object instanceof ConfigurableInterface) {
