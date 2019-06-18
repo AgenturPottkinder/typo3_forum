491830e4a59303581099b2c0089ae5394f91e41b
Stephan Großberndt <s.grossberndt@sidebysite.de>
2019-06-18 13:46:04 +0200

    Move addStaticFile calls to TCA/Overrides/sys_template.php

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,13 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+	'typo3_forum',
+	'Configuration/TypoScript',
+	'typo3_forum'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+	'typo3_forum',
+	'Configuration/TypoScript/Bootstrap',
+	'typo3_forum Bootstrap Template'
+);
