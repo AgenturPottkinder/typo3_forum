a2aff0cdbe411e766fa84c3aabfee2fed50c68dd
Bastian Bringenberg <bastian@agentur-pottkinder.de>
2020-10-28 14:48:24 +0100

    First Steps of v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,9 +4,9 @@ $EM_CONF[$_EXTKEY] = [
 	'title' => 'typo3_forum',
 	'description' => 'Forum extension',
 	'category' => 'plugin',
-	'author' => 'Mittwald CM Service',
-	'author_email' => 'support@mittwald.de',
-	'author_company' => 'Mittwald CM Service',
+	'author' => 'Agentur Pottkinder',
+	'author_email' => 'support@agentur-pottkinder.de',
+	'author_company' => 'Agentur Pottkinder',
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => 'typo3temp/typo3_forum,typo3temp/typo3_forum/gravatar, typo3temp/typo3_forum/workflowstatus',
@@ -18,9 +18,9 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '1.0-dev',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '8.7.0 - 9.5.99',
+			'typo3' => '10.4',
 			'static_info_tables' => '',
-			'php' => '7.1',
+			'php' => '7.2',
 		]
 	],
 ];
